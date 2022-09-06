c8b436e0632886146f450279355ad1b6baa6f8a0
Julius Range-Lüdemann <julius.range@gmail.com>
2022-09-06 20:56:31 +0200

    Make sed work on macos

--- build_export.sh
+++ build_export.sh
@@ -1,39 +1,52 @@
 #! /bin/sh
 
+copy_file () {
+    echo "Copying '$1'"
+    cp "${WEB_PATH}/$1" "${EXPORT_PATH}/$1"
+}
+
 REL_PATH=$(dirname "$0")
 WEB_PATH="${REL_PATH}/web"
 EXPORT_PATH="${REL_PATH}/export"
 
 DOMAIN_NAME="$1"
 
+if [ -z "${DOMAIN_NAME}" ]; then
+    echo "There was no domain name specified. You might want to run it with one:"
+    echo "  $0 <domain-name>"
+    echo
+fi
+
 if [ ! -d "${EXPORT_PATH}" ]; then
     mkdir -p "${EXPORT_PATH}"
 fi
 
-cp "${WEB_PATH}/apple-touch-icon.png" "${EXPORT_PATH}/apple-touch-icon.png"
-cp "${WEB_PATH}/favicon.ico" "${EXPORT_PATH}/favicon.ico"
-cp "${WEB_PATH}/favicon.svg" "${EXPORT_PATH}/favicon.svg"
-cp "${WEB_PATH}/icon.svg" "${EXPORT_PATH}/icon.svg"
-cp "${WEB_PATH}/icon192.png" "${EXPORT_PATH}/icon192.png"
-cp "${WEB_PATH}/icon512.png" "${EXPORT_PATH}/icon512.png"
-cp "${WEB_PATH}/icon512-maskable.png" "${EXPORT_PATH}/icon512-maskable.png"
+copy_file "apple-touch-icon.png"
+copy_file "favicon.ico"
+copy_file "favicon.svg"
+copy_file "icon.svg"
+copy_file "icon192.png"
+copy_file "icon512.png"
+copy_file "icon512-maskable.png"
 
-cp "${WEB_PATH}/manifest.webmanifest" "${EXPORT_PATH}/manifest.webmanifest"
-cp "${WEB_PATH}/style.css" "${EXPORT_PATH}/style.css"
-cp "${WEB_PATH}/common.js" "${EXPORT_PATH}/common.js"
-cp "${WEB_PATH}/main_worker_thread.js" "${EXPORT_PATH}/main_worker_thread.js"
-cp "${WEB_PATH}/training_planner.js" "${EXPORT_PATH}/training_planner.js"
+copy_file "manifest.webmanifest"
+copy_file "style.css"
+copy_file "common.js"
+copy_file "main_worker_thread.js"
+copy_file "training_planner.js"
+
+echo "Copying 'index.html'"
 
 if [ ! -z "${DOMAIN_NAME}" ]; then
     cat "${WEB_PATH}/index.html" | \
-        sed "s/\[domain_name\]/${DOMAIN_NAME}/" | \
-        sed 's/<!-- <meta\(.*\) -->/<meta\1/g' | \
-        sed 's/^[ ]\+//g' | \
+        sed -E "s/\[domain_name\]/${DOMAIN_NAME}/" | \
+        sed -E 's/<!-- <meta(.*) -->/<meta\1/g' | \
+        sed -E 's/^[ ]+//g' | \
         tr -d '\n' > "${EXPORT_PATH}/index.html"
 else
     cat "${WEB_PATH}/index.html" | \
-        sed 's/<!--.*-->//g' | \
-        sed 's/^[ ]\+//g' | \
-        sed '/^[ ]*$/d' | \
+        sed -E 's/<!--.*-->//g' | \
+        sed -E 's/^[ ]+//g' | \
+        sed -E '/^[ ]*$/d' | \
         tr -d '\n' > "${EXPORT_PATH}/index.html"
 fi
